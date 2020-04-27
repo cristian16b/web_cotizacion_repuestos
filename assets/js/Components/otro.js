339a23b1397ff055ec8df5eef1ad670d86e71372
cristian16b <cristian.budzicz@gmail.com>
2020-04-27 12:14:02 -0300

    Logrado incorporar otro componente dentro del componente raiz

---
+++ assets/js/Components/otro.js
@@ -0,0 +1,16 @@
+import React , { Component } from 'react';
+
+class Otro extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Mensaje desde React, otra prueba!</h1>
+    );
+  }
+}
+
+export default Otro;
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Otro from './Components/otro';
 require("../css/app.css");
  
+
+class App extends React.Component {
+
+  // ref: https://gist.github.com/darklilium/183ce1405788f2aef7e8
+    render() {
+      return (
+        <div>
+          <h1>Hola soy el amigo</h1>
+          <Otro></Otro>
+        </div>
+      );
+    }
+}
+
 ReactDOM.render(
-    <h1>Mensaje desde React!</h1>,
+    <App />,
     document.getElementById('root')
 );
