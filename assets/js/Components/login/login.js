25c2d82ec0ee0210f3a1df757d1f27a8e222e038
cristian16b <cristian.budzicz@gmail.com>
2020-05-04 16:51:06 -0300

    Se agrego template del login; to-do modificar labels

--- assets/js/Components/login/login.js
+++ assets/js/Components/login/login.js
@@ -1,4 +1,5 @@
 import React , { Component } from 'react';
+require("../login/login.css");
 
 class Login extends React.Component {
 
@@ -8,27 +9,42 @@ class Login extends React.Component {
 
   render() {
     return (
-        <div className="wrapper fadeInDown">
-            <div id="formContent">
-                {/* <!-- Tabs Titles --> */}
-            
-                {/* <!-- Icon --> */}
-                <div className="fadeIn first">
-                    <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+        <div className="login-form">
+            <form action="/examples/actions/confirmation.php" method="post">
+                <h2 className="text-center">Sign in</h2>   
+                <div className="form-group">
+                    <div className="input-group">
+                        <span className="input-group-addon"><i className="fa fa-user"></i></span>
+                        {/* importante los elementos input deben terminar así: <input /> y no <input></input> porque genera error */}
+                        <input type="text" className="form-control" name="username" placeholder="Username" required="required" />
+                        {/* </input>				 */}
+                    </div>
                 </div>
-        
-                {/* <!-- Login Form --> */}
-                <form>
-                    <input type="text" id="login" className="fadeIn second" name="login" placeholder="login"></input>
-                    <input type="text" id="password" className="fadeIn third" name="login" placeholder="password"></input>
-                    <input type="submit" className="fadeIn fourth" value="Log In"></input>
-                </form>
-               
-                {/* <!-- Remind Passowrd --> */}
-                <div id="formFooter">
-                    <a className="underlineHover" href="#">Forgot Password?</a>
+                <div className="form-group">
+                    <div className="input-group">
+                        <span className="input-group-addon"><i className="fa fa-lock"></i></span>
+                        <input type="password" className="form-control" name="password" placeholder="Password" required="required" />	
+                        {/* </input>			 */}
+                    </div>
+                </div>        
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary login-btn btn-block">Sign in</button>
                 </div>
-            </div>
+                <div className="clearfix">
+                    <label className="pull-left checkbox-inline"><input type="checkbox" /> Remember me</label>
+                    <a href="#" className="pull-right">Forgot Password?</a>
+                </div>
+                <div className="or-seperator">
+                    <i>or</i>
+                </div>
+                <p className="text-center">Login with your social media account</p>
+                <div className="text-center social-btn">
+                    <a href="#" className="btn btn-primary"><i className="fa fa-facebook"></i>&nbsp; Facebook</a>
+                    <a href="#" className="btn btn-info"><i className="fa fa-twitter"></i>&nbsp; Twitter</a>
+                    <a href="#" className="btn btn-danger"><i className="fa fa-google"></i>&nbsp; Google</a>
+                </div>
+            </form>
+            <p className="text-center text-muted small">Don't have an account? <a href="#">Sign up here!</a></p>
         </div>
     );
   }
