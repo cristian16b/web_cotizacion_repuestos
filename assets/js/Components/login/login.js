1e258b3c690799e3f4a55974b0740d1856d3aad3
cristian16b <cristian.budzicz@gmail.com>
2020-05-05 09:41:31 -0300

    Agregamos elementos en el login

--- assets/js/Components/login/login.js
+++ assets/js/Components/login/login.js
@@ -4,63 +4,91 @@ import {Link}  from 'react-router-dom'
 
 class Login extends React.Component {
 
-  constructor(props){
-    super(props);
-  }
+    constructor(props){
+        super(props);
 
-  render() {
-    return (
-        <div className="row justify-content-center">
-            <div className="col-lg-5">
-                <form action="/examples/actions/confirmation.php" method="post">
-                    <h2 className="text-center">Iniciar sesión</h2>   
-                    <div className="form-group">
-                        <div className="input-group">
-                            <span className="input-group-addon"><i className="fa fa-user"></i></span>
-                            {/* importante los elementos input deben terminar así: <input /> y no <input></input> porque genera error */}
-                            <input type="text" className="form-control" name="username" 
-                                    placeholder="Ingrese su nombre de usuario" required="required" />
+        this.state = ({
+            username:'',
+            password:''
+        })
+
+        this.cambioUsername = this.cambioUsername.bind(this);
+        this.cambioPassword = this.cambioPassword.bind(this);
+        this.handleSubmit   = this.handleSubmit.bind(this); 
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.username + ' ' + this.state.password);
+        event.preventDefault();
+    }
+
+    cambioPassword(e) {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    cambioUsername(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="row justify-content-center">
+                <div className="col-lg-5">
+                    <form action="/examples/actions/confirmation.php" method="post" onSubmit={this.handleSubmit}>
+                        <h2 className="text-center">Iniciar sesión</h2>   
+                        <div className="form-group">
+                            <div className="input-group">
+                                <span className="input-group-addon"><i className="fa fa-user"></i></span>
+                                {/* importante los elementos input deben terminar así: <input /> y no <input></input> porque genera error */}
+                                <input type="text" className="form-control" name="username" 
+                                        defaultValue={this.state.username} onChange={this.cambioUsername}
+                                        placeholder="Ingrese su nombre de usuario" required="required" />
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="input-group">
-                            <span className="input-group-addon"><i className="fa fa-lock"></i></span>
-                            <input type="password" className="form-control" name="password" 
-                                    placeholder="Ingrese su contraseña" required="required" />	
+                        <div className="form-group">
+                            <div className="input-group">
+                                <span className="input-group-addon"><i className="fa fa-lock"></i></span>
+                                <input type="password" className="form-control" name="password" 
+                                        defaultValue = {this.state.password} onChange={this.cambioPassword}
+                                        placeholder="Ingrese su contraseña" required="required" />	
+                            </div>
+                        </div>        
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary login-btn btn-block">Ingresar</button>
                         </div>
-                    </div>        
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary login-btn btn-block">Ingresar</button>
-                    </div>
-                    <div className="clearfix">
-                        {/* <label className="pull-left checkbox-inline"><input type="checkbox" /> Remember me</label> */}
+                        <div className="clearfix">
+                            {/* <label className="pull-left checkbox-inline"><input type="checkbox" /> Remember me</label> */}
+                            <Link to="/registrarme" className="navbar-brand">
+                                <span className="small"> 
+                                    ¿Olvidaste tu contraseña?
+                                </span>
+                            </Link>
+                        </div>
+                        <div className="clearfix">
+                            <hr />
+                        </div>
+                        <p className="text-center">O ingresar con tu red social</p>
+                        <div className="text-center social-btn">
+                            <a href="#" className="btn btn-primary"><i className="fa fa-facebook"></i>&nbsp; Facebook</a>
+                            {/* <a href="#" className="btn btn-info"><i className="fa fa-twitter"></i>&nbsp; Twitter</a>
+                            <a href="#" className="btn btn-danger"><i className="fa fa-google"></i>&nbsp; Google</a> */}
+                        </div>
+                    </form>
+                    <p className="text-center text-muted small">¿No tienes una cuenta? 
                         <Link to="/registrarme" className="navbar-brand">
                             <span className="small"> 
-                                ¿Olvidaste tu contraseña?
+                                Registrate!
                             </span>
                         </Link>
-                    </div>
-                    <div className="clearfix">
-                        <hr />
-                    </div>
-                    <p className="text-center">O ingresar con tu red social</p>
-                    <div className="text-center social-btn">
-                        <a href="#" className="btn btn-primary"><i className="fa fa-facebook"></i>&nbsp; Facebook</a>
-                        {/* <a href="#" className="btn btn-info"><i className="fa fa-twitter"></i>&nbsp; Twitter</a>
-                        <a href="#" className="btn btn-danger"><i className="fa fa-google"></i>&nbsp; Google</a> */}
-                    </div>
-                </form>
-                <p className="text-center text-muted small">¿No tienes una cuenta? 
-                    <Link to="/registrarme" className="navbar-brand">
-                        <span className="small"> 
-                            Registrate!
-                        </span>
-                    </Link>
-                </p>
+                    </p>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Login;
