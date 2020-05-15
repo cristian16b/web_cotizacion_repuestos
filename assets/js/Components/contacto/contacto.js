13e4320477164a5b71af8a2ebc4a5e9dcc76df03
cristian16b <cristian.budzicz@gmail.com>
2020-05-15 11:04:33 -0300

    Se agrego formulario de login

--- assets/js/Components/contacto/contacto.js
+++ assets/js/Components/contacto/contacto.js
@@ -10,7 +10,101 @@ class Contacto extends React.Component {
     return (        
       <div className="row justify-content-center">
         <div className="col-lg-9">
-          <h1 className="my-4">Formulario de contacto</h1>
+          
+            <div className="card">
+              <div className="card-body">
+              <section className="mb-4">
+                <h1 className="my-4">Formulario de contacto</h1>
+                <p className="text-center w-responsive mx-auto mb-5">¿Tenes alguna consulta?</p>
+
+                <div className="row">
+
+                    {/* <!--Grid column--> */}
+                    <div className="col-md-9 mb-md-0 mb-5">
+                        <form id="contact-form" name="contact-form">
+
+                            {/* <!--Grid row--> */}
+                            <div className="row">
+
+                                {/* <!--Grid column--> */}
+                                <div className="col-md-6">
+                                    <div className="md-form mb-0">
+                                      <label forhtml="name" className="">Nombre y apellido</label>
+                                      <input type="text" id="name" name="name" className="form-control" />
+                                    </div>
+                                </div>
+                                {/* <!--Grid column--> */}
+
+                                {/* <!--Grid column--> */}
+                                <div className="col-md-6">
+                                    <div className="md-form mb-0">
+                                      <label forhtml="email" className="">Email</label>
+                                      <input type="text" id="email" name="email" className="form-control" />
+                                    </div>
+                                </div>
+                                {/* <!--Grid column--> */}
+                            </div>
+                            {/* <!--Grid row-->
+
+                            <!--Grid row--> */}
+                            <div className="row">
+
+                                {/* <!--Grid column--> */}
+                                <div className="col-md-12">
+
+                                    <div className="md-form">
+                                        <label forhtml="message">Tu pregunta</label>
+                                        <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea"></textarea>
+                                        
+                                    </div>
+
+                                </div>
+                            </div>
+                            {/* <!--Grid row--> */}
+                            <div className="row">
+
+                                {/* <!--Grid column--> */}
+                                <div className="col-md-6">
+
+                                    <div className="md-form">
+                                      <br />
+                                      <button type="submit" className="btn btn-primary login-btn btn-block">Enviar</button>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </form>
+
+                        
+                        
+                        <div className="status"></div>
+                    </div>
+                    {/* <!--Grid column--> */}
+
+                    {/* <!--Grid column--> */}
+                    <div className="col-md-3 text-center">
+                        <ul className="list-unstyled mb-0">
+                            <li><i className="fas fa-map-marker-alt fa-2x"></i>
+                                <p>Santa fe ...</p>
+                            </li>
+
+                            <li><i className="fas fa-phone mt-4 fa-2x"></i>
+                                <p>342 .....</p>
+                            </li>
+
+                            <li><i className="fas fa-envelope mt-4 fa-2x"></i>
+                                <p>contacto@correo.com</p>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* <!--Grid column--> */}
+
+                </div>
+
+                </section>
+              </div>
+            </div>
         </div>
       </div>
     );
