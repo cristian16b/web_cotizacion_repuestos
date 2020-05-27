af6f0610adf22300dfd237e2694da60c0488945c
cristian16b <cristian.budzicz@gmail.com>
2020-05-27 12:16:43 -0300

    Modificado color navbar

--- assets/js/Components/barra superior/navBarNoLogueado.js
+++ assets/js/Components/barra superior/navBarNoLogueado.js
@@ -8,7 +8,7 @@ class NavbarNoLogueado extends React.Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar-custom navbar navbar-expand-lg">
                 <Link to="/" className="navbar-brand"><span className="navbar-brand"></span></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
