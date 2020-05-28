2d8f861b25114c1d414c87546c7c26ddfb78153b
cristian16b <cristian.budzicz@gmail.com>
2020-05-28 08:44:10 -0300

    Se modifican las claves publicas/privadas del recaptcha, se agrego la constante para la url base

--- assets/js/Constantes/constantes.js
+++ assets/js/Constantes/constantes.js
@@ -1,6 +1,7 @@
-export const API_BASE_URL = 'http://localhost/web_cotizacion_repuestos/public';
+// export const API_BASE_URL = 'http://localhost/web_cotizacion_repuestos/public';
+export const API_BASE_URL = 'http://www.eisenparts.com';
 export const API_LOGIN = API_BASE_URL + '/api/login_check';
 export const API_LOGIN_SOCIAL = API_BASE_URL + '/api/login_social';
 export const API_REGISTER = API_BASE_URL + '/api/register';
-export const API_CAPTCHA_PUBLIC = '6Lfs3vUUAAAAAFhViJoef2WX1fGsmL7uVEU4B9Vj';
-export const API_CAPTCHA_PRIVATE = '6Lfs3vUUAAAAACrmMA-u9ktm6d5j1vwZFbS8Y2kE';
+export const API_CAPTCHA_PUBLIC = '6Ld2A_0UAAAAADDGvROkBFyPOPDkZWqoLLkbaWym';
+export const API_CAPTCHA_PRIVATE = '6Ld2A_0UAAAAALQJE4OHpHCktkGQRsIwbuzZKDZ4';
