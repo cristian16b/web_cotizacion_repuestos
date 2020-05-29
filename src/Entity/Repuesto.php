e0411ca3bd1d9d1a4ef453abc4b4c3b1b6f68120
cristian16b <cristian.budzicz@gmail.com>
2020-05-29 13:05:23 -0300

    funcionando correctamente la api de repuestos por nombre

--- src/Entity/Repuesto.php
+++ src/Entity/Repuesto.php
@@ -6,10 +6,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use DateTime;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RepuestoRepository")
  * @ORM\HasLifecycleCallbacks()
+ * @ExclusionPolicy("all")
  */
 class Repuesto
 {
@@ -17,11 +20,13 @@ class Repuesto
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Expose
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Expose
      */
     private $name;
 
