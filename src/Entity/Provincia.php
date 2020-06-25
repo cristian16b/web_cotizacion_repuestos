77791dbac230c29a374e77cf432f335a96506099
cristian16b <cristian.budzicz@gmail.com>
2020-06-25 13:37:22 -0300

    agregado los excludes a la entidad provincia

--- src/Entity/Provincia.php
+++ src/Entity/Provincia.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProvinciaRepository")
+ * @ExclusionPolicy("all")
  */
 class Provincia
 {
@@ -15,11 +18,13 @@ class Provincia
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Expose
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Expose
      */
     private $provincia;
 
