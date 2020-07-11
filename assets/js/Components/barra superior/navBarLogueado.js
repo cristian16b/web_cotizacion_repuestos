70a4c5e7a940d265d7a1e0ecb90d6cb31e4d3740
cristian16b <cristian.budzicz>
2020-07-11 20:33:51 -0300

    Modificado el orden de los items en el navbar de usuario logueado

--- assets/js/Components/barra superior/navBarLogueado.js
+++ assets/js/Components/barra superior/navBarLogueado.js
@@ -27,26 +27,12 @@ class NavbarLogueado extends React.Component {
                         <li data-toggle="collapse" data-target=".navbar-collapse"  className={this.getNavLinkClass("/repuesto")}>
                             <Link to="/repuesto" className="navbar-brand"><span className="nav-link">Buscar un repuesto</span></Link>
                         </li>
-                        {/* nota: react por algun motivo raro (no se cual) no acepta e anidamiento de listas li (una dentro de otra) */}
-                        <div className="nav-item dropdown">
-                            <li data-toggle="collapse" className="dropdown-toggle" id="navbarDropdownMenuLink"  data-toggle="dropdown" >
-                                <Link to="/perfil" className="navbar-brand"><span className="nav-link">Mis datos</span></Link>
-                            </li>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/cotizaciones")}>
-                                    <Link to="/cotizaciones" className="navbar-brand"><span className="nav-link nav-link-blanco">Mis cotizaciones</span></Link>
-                                </li>
-                                <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/perfil")}>
-                                    <Link to="/perfil" className="navbar-brand"><span className="nav-link nav-link-blanco">Mi perfil</span></Link>
-                                </li>
-                            </div>
-                        </div>
-                        {/* <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/login")}>
-                            <Link to="/login" className="navbar-brand"><span className="nav-link">Ingresar</span></Link>
+                        <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/cotizaciones")}>
+                            <Link to="/cotizaciones" className="navbar-brand"><span className="nav-link">Mis cotizaciones</span></Link>
                         </li>
-                        <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/registrarme")}>
-                            <Link to="/registrarme" className="navbar-brand"><span className="nav-link">Registrarme</span></Link>
-                        </li> */}
+                        <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/perfil")}>
+                            <Link to="/perfil" className="navbar-brand"><span className="nav-link">Mi perfil</span></Link>
+                        </li>
                         <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/contacto")}>
                             <Link to="/contacto" className="navbar-brand"><span className="nav-link">¿Dudas, Consultas?</span></Link>
                         </li>
