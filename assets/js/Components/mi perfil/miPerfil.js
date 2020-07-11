5febc2d10e809e2362e2bd91655a57a6b41edc3b
cristian16b <cristian.budzicz>
2020-07-11 17:08:55 -0300

    Trabajando con mi perfil

--- assets/js/Components/mi perfil/miPerfil.js
+++ assets/js/Components/mi perfil/miPerfil.js
@@ -1,16 +1,35 @@
 import React , { Component } from 'react';
+import Loading from '../loading/loading.js';
 
 class MiPerfil extends React.Component {
 
   constructor(props){
     super(props);
+
+    this.state = ({
+      menu: false,
+      isMount: false,
+      isLoading: true, // inicialmente esta cargando hasta que se monta el componente en componentdidmount()
+      misSolicitudes: [],
+      repuestoBuscar: '',
+      errors:{},
+    });
+
   }
 
   render() {
+    // if(this.state.isLoading == true)
+    //   return  <Loading></Loading>
     return (        
-      <div className="row justify-content-center shadow-sm p-3 mb-5 bg-white rounded">
+      <div className="row justify-content-center">
         <div className="col-12 col-sm-12 col-md-12 col-lg-9">
-          <h3 className="my-4">Mi perfil</h3>
+            <div className="card shadow-sm p-3 mb-5 bg-white rounded">
+              <div className="card-body">
+                <h3 className="my-4">Mi perfil</h3>
+                <hr/>
+
+            </div>
+          </div>
         </div>
       </div>
     );
