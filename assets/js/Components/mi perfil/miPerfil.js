abf842f4c5cddfafe61a76ba266e8129c41b1fc7
cristian16b <cristian.budzicz>
2020-07-12 12:01:23 -0300

    Se agregaron los datos a mi perfil,to-do modificar los componentes de formularos datos comunes y comerciantes para que sean solo lectura

--- assets/js/Components/mi perfil/miPerfil.js
+++ assets/js/Components/mi perfil/miPerfil.js
@@ -1,5 +1,7 @@
 import React , { Component } from 'react';
 import {ROL_COMERCIANTE,ROL_USER} from '../../Constantes/constantes';
+import FormularioDatosComunes from '../registrarme/FormularioDatosComunes.js';
+import FormularioDatosComerciante from '../registrarme/FormularioDatosComerciante.js';
 
 class MiPerfil extends React.Component {
 
@@ -7,20 +9,89 @@ class MiPerfil extends React.Component {
     super(props);
 
     this.state = ({
+      nombre:'',
+      apellido: '',
+      codArea: '',
+      telefono: '',
+      email: '',
+      //solo para vendedores
+      esComerciante: false,
+      cuitCuit: '',
+      // datos del comerciante
+      constanciaDni: '',
+      constanciaAfip: '',
+      calle: '',
+      nro: '',
+      localidad: '',
+      provincia: '',
+      //
+      password:'',
+      password2: '',
+      // errores locales o retornados por la api
+      errors: {},
+      errorApi: '',
     });
 
   }
 
   renderDatosPerfil() {
-    console.log(this.props.rol);
-    if(this.props.rol == ROL_COMERCIANTE) {
-
+    if(this.props.rol[0] == ROL_COMERCIANTE) {
+      return(
+        <>
+          <FormularioDatosComunes 
+              handleChangeInput={this.handleChangeInput} 
+              errors={this.state.errors}
+              errorsApi={this.state.errorApi}
+              nombre={this.state.nombre}
+              apellido={this.state.apellido}
+              codArea={this.state.codArea}
+              telefono={this.state.telefono}
+              email={this.state.email}
+              password={this.state.password}
+              password2={this.state.password2}
+            >
+          </FormularioDatosComunes>
+          <hr></hr>
+          <FormularioDatosComerciante
+              handleChangeInput={this.handleChangeInput} 
+              handleChangeSelectLocalidad={this.handleChangeSelectLocalidad}
+              handleChangeSelectProvincia={this.handleChangeSelectProvincia}
+              constanciaDni={this.state.constanciaDni}
+              constanciaAfip={this.state.constanciaAfip}
+              calle={this.props.calle}
+              nro={this.props.nro}
+              provincia={this.state.provincia}
+              localidad={this.state.localidad}
+              errors={this.state.errors}
+              errorsApi={this.state.errorApi}
+              onChangeValueDni={this.handleChangeValueArchivoDni}
+              onChangeValueAfip={this.handleChangeValueArchivoAfip}
+            >
+            </FormularioDatosComerciante>        
+        </>
+      );
     }
-    else if(this.props.rol == ROL_USER) {
-
+    else if(this.props.rol[0] == ROL_USER) {
+      return(
+        <FormularioDatosComunes 
+            handleChangeInput={this.handleChangeInput} 
+            errors={this.state.errors}
+            errorsApi={this.state.errorApi}
+            nombre={this.state.nombre}
+            apellido={this.state.apellido}
+            codArea={this.state.codArea}
+            telefono={this.state.telefono}
+            email={this.state.email}
+            password={this.state.password}
+            password2={this.state.password2}
+          >
+        </FormularioDatosComunes>
+      );
     }
     else {
-
+      return(
+null
+      );
     }
   }
 
