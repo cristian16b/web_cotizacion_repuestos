e1f494e4baaf392bb5e4c2d3483b88db53ea825c
cristian16b <cristian.budzicz@gmail.com>
2020-07-15 20:29:22 -0300

    Se modifico el loading, ahora se puede visualizar correctamente

--- assets/js/Components/loading/loading.js
+++ assets/js/Components/loading/loading.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
  
+const divLoading = {
+    'height': "100%",
+    'width': "100%",   
+}
+
+const divBucle = {
+    'margin': '1% auto'
+}
+
+const divMensaje = {
+    'margin': '6% auto'
+}
+
+
 const Loading = () => (
-    <div className="row justify-content-center">
-            <div className="col-6 col-md-6 col-lg-6">
-                <ReactLoading type={'spin'} color="#153D57" height={'30%'} width={'30%'}/>
+    <div className="row justify-content-center" style={divLoading}>
+            <div className="row justify-content-center"  align="center" style={divBucle}>
+                <div className="col-12 col-md-6 col-lg-6">
+                    <ReactLoading type={'spin'} color="#153D57" height={'35%'} width={'35%'}/>
+                </div>
             </div>
-            <div className="col-12 col-md-8 col-lg-8">
-                <h3>Cargando...Espere un momento...</h3>
+            <div className="row justify-content-center" align="center" style={divMensaje}>
+                <div className="col-12 col-md-8 col-lg-10">
+                    <h4>Cargando</h4>
+                </div>
+                <div className="col-12 col-md-8 col-lg-10">
+                    <h4>...</h4>
+                </div>
+                <div className="col-12 col-md-8 col-lg-10">
+                    <h4>Espere un momento</h4>
+                </div>
             </div>
     </div>
 );
