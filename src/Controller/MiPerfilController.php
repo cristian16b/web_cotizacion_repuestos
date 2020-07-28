18b44f38e462a606c9e00e83f6c4585e394a3ee3
cristian16b <cristian.budzicz@gmail.com>
2020-07-28 10:09:27 -0300

    Se empieza a trabajar con el backend para conseguir los datos de mi perfil

--- src/Controller/MiPerfilController.php
+++ src/Controller/MiPerfilController.php
@@ -5,15 +5,43 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api/v1/perfil")
+*/
 class MiPerfilController extends AbstractController
 {
-    /**
-     * @Route("/mi/perfil", name="mi_perfil")
-     */
-    public function index()
+    // /**
+    //  * @Route("/mi/perfil", name="mi_perfil")
+    //  */
+    // public function index()
+    // {
+    //     return $this->render('mi_perfil/index.html.twig', [
+    //         'controller_name' => 'MiPerfilController',
+    //     ]);
+    // }
+
+    public static function getSubscribedServices() 
     {
-        return $this->render('mi_perfil/index.html.twig', [
-            'controller_name' => 'MiPerfilController',
-        ]);
+        return array_merge(parent::getSubscribedServices(), [ 'jms_serializer' => '?'.SerializerInterface::class, ]); 
+    }
+
+    /**
+     * @Rest\Get("/getByName", name="marca_auto_getAll", defaults={"_format":"json"})
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Retorna todos los repuestos."
+     * )
+     *
+     * @SWG\Response(
+     *     response=500,
+     *     description="Un error ocurrio en la busqueda de los repuestos."
+     * )
+     *
+     * @SWG\Tag(name="Repuesto")
+     */
+    public function getByNameAction(Request $request)
+    {
+
     }
 }
