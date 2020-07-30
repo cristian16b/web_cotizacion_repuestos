517006a953b27cb15799c8fde2c19740038338e8
cristian16b <cristian.budzicz@gmail.com>
2020-07-30 09:29:37 -0300

    Se genero la posibilidad de descargar los pdf/images desde el perfil

--- src/Controller/RecursoController.php
+++ src/Controller/RecursoController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\ConstanciaPersona;
 
 /**
 * @Route("/recurso")
@@ -40,6 +41,52 @@ class RecursoController extends AbstractController
         
         if(file_exists($filepath)){
             $response->headers->set('Content-Type', 'image/' . $extension);
+            $response->setContent(file_get_contents($filepath));
+        }
+        else {
+            throw new \Exception('Something went wrong!');
+        }
+        return $response;
+    }
+
+   /**
+    * @Route("/get/archivo/{nombreArchivo}", name="get_recurso_archivo")
+    */
+    public function getArchivoAction(Request $request,$nombreArchivo) {
+
+        if(is_null($nombreArchivo)) {
+            throw new \Exception('Something went wrong!');
+        }
+
+        // busco constancia
+        $archivo = $this->getDoctrine()->getRepository(ConstanciaPersona::class)->findOneBy(
+            [
+                'nombreFisico' => $nombreArchivo
+            ]
+        );
+
+        $filepath =  $this->getParameter('kernel.project_dir') . '/constancias/' .  $archivo->getTipo()->getNombre() . 
+                        '/' . $nombreArchivo;
+
+        $extension = substr($nombreArchivo,strripos($nombreArchivo, '.'));
+
+        $response = new Response();
+        
+        if(file_exists($filepath)){
+
+            if($extension == '.jpeg') {
+                $response->headers->set('Content-Type', 'image/jpeg');
+            }
+            else if($extension == '.png') {
+                $response->headers->set('Content-Type', 'image/png');
+            }
+            else if($extension == '.pdf') {
+                $response->headers->set('Content-Type', 'application/pdf');
+            }
+            else {
+                throw new \Exception('Something went wrong!');
+            }
+
             $response->setContent(file_get_contents($filepath));
         }
         else {
