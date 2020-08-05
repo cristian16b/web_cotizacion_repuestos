ce5973a2467597f545c4f2b6bf82d68210bfa1fe
cristian16b <cristian.budzicz@gmail.com>
2020-08-05 09:06:47 -0300

    Se esta agregando la funcionalidad para registrar la cotizacion

--- src/Controller/CotizacionesController.php
+++ src/Controller/CotizacionesController.php
@@ -4,19 +4,97 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use JMS\Serializer\SerializerInterface;
 /**
 * @Route("/cotizaciones")
 */
 class CotizacionesController extends AbstractController
 {
-    /**
-     * @Route("/", name="mis_cotizaciones")
-     */
-    public function index()
+    // /**
+    //  * @Route("/", name="mis_cotizaciones")
+    //  */
+    // public function index()
+    // {
+    //     return $this->render('mis_cotizaciones/index.html.twig', [
+    //         'controller_name' => 'MisCotizacionesController',
+    //     ]);
+    // }
+
+    public static function getSubscribedServices() 
     {
-        return $this->render('mis_cotizaciones/index.html.twig', [
-            'controller_name' => 'MisCotizacionesController',
-        ]);
+        return array_merge(parent::getSubscribedServices(), [ 'jms_serializer' => '?'.SerializerInterface::class, ]); 
+    }
+
+       /**
+     * @Rest\Get("/enviar", name="cotizaciones_enviar", defaults={"_format":"json"})
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="La cotizacion fue enviada."
+     * )
+     *
+     * @SWG\Response(
+     *     response=500,
+     *     description="Un error ocurrio al enviar la cotizacion."
+     * )
+     *
+     * @SWG\Tag(name="Repuesto")
+     */
+    public function enviarCotizacionAction(Request $request)
+    {
+        $serializer = $this->container->get('jms_serializer');
+        $repuestos = [];
+        $message = "";
+
+        try {
+            $code = 200;
+            $error = false;
+
+            $idSolicitud = $request->request->get('idSolicitud');
+            $monto = $request->request->get('monto');
+
+            dump($idSolicitud);dump($monto);die;
+
+
+            if(is_null($nombreArchivo)) {
+                throw new \Exception('Something went wrong!');
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $repuestos= $em->getRepository(MarcaAuto::class)
+                    ->buscarPorNombre($nameIngresado);
+
+            if (is_null($repuestos)) {
+                $repuestos = [];
+            }
+
+        } catch (Exception $ex) {
+            $code = 500;
+            $error = true;
+            $message = "Error: {$ex->getMessage()}";
+        }
+
+        $response = [
+            'code' => $code,
+            'error' => $error,
+            'data' => $code == 200 ? $repuestos : $message,
+        ];
+        
+        return new Response(
+            $serializer->serialize(
+                $response,
+                "json"
+            )
+        );
     }
 }
