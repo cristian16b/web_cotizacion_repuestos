4ba1de25df915bc7c8313fc2228ed6524ddff4e0
cristian16b <cristian.budzicz@gmail.com>
2020-08-13 18:58:49 -0300

    Se agrego funcionalidades para empezar a trabajar el cambio de pass en el backend

--- src/Controller/MiPerfilController.php
+++ src/Controller/MiPerfilController.php
@@ -101,4 +101,88 @@ class MiPerfilController extends AbstractController
             )
         );
     }
+
+     /**
+     * @Rest\Post("/cambiarContrasenia", name="perfil_cambiar_contrasenia")
+     *
+     * @SWG\Response(
+     *     response=201,
+     *     description="El pedido ha sido registrado."
+     * )
+     *
+     * @SWG\Response(
+     *     response=500,
+     *     description="El pedido pude ser registrado."
+     * )
+     * 
+     * @SWG\Parameter(
+     *     name="_email",
+     *     in="body",
+     *     type="string",
+     *     description="The username",
+     *     schema={}
+     * )
+     *
+     * @SWG\Parameter(
+     *     name="_password",
+     *     in="query",
+     *     type="string",
+     *     description="The password"
+     * )
+     *
+     * @SWG\Tag(name="Cambiar password ")
+     */
+    public function cambiarContraseniaAction(Request $request, UserPasswordEncoderInterface $encoder,ValidatorInterface $validator) {
+        $serializer = $this->container->get('jms_serializer');
+        $em = $this->getDoctrine()->getManager();
+ 
+        $user = [];
+        $message = "";
+ 
+        try {
+
+            $code = 200;
+            $error = false;
+
+            // $email = $request->request->get('email');
+            // $contraseniaI = $request->request->get('contrasenia');
+            // $contraseniaII = $request->request->get('contrasenia');
+
+            dump($request->request);
+            die;
+ 
+            $repuesto = $this->obtenerRepuesto($idRepuesto);
+            $modelo = $this->obtenerModeloAuto($idModelo);
+            $marca = $this->obtenerMarcaAuto($idMarca);
+
+            if(is_null($repuesto) || is_null($marca) || is_null($modelo) || is_null($imagenes)) {
+                throw new \Exception('Something went wrong!');
+            }
+
+
+            if($formErrors) {
+                $response = [
+                    'code' => 0,
+                    'error' => $formErrors,
+                ];
+                return new JsonResponse($response);
+            }
+
+            $em->persist($solicitud);
+            $em->flush();
+ 
+        } catch (Exception $ex) {
+            $code = 500;
+            $error = true;
+            $message = "Ocurrio un error al intentar agregar al usuario - Error: {$ex->getMessage()}";
+        }
+ 
+        $response = [
+            'code' => $code,
+            'error' => $error,
+            // 'data' => $code == 200 ? $user : $message,
+        ];
+ 
+        return new Response($serializer->serialize($response, "json"));
+    }
 }
