30840cddb5336612a1d54a8b9e4239f6a9d71300
cristian16b <cristian.budzicz@gmail.com>
2020-08-13 18:13:25 -0300

    Se agregaron el captcha y los botones.todo acomodar la grilla y empezar a trabajar con las funcionalidades

--- assets/js/Components/recuperar/recuperarContrasenia.js
+++ assets/js/Components/recuperar/recuperarContrasenia.js
@@ -1,4 +1,9 @@
 import React , { Component } from 'react';
+import {API_REGISTER,API_CAPTCHA_PUBLIC} from '../../Constantes/constantes';
+import axios from 'axios';
+import ReCAPTCHA from "react-google-recaptcha";
+import Loading from '../loading/loading.js';
+import { Redirect } from 'react-router-dom';
 
 class RecuperarContrasenia extends React.Component {
 
@@ -19,6 +24,8 @@ class RecuperarContrasenia extends React.Component {
     this.handleSubmit   = this.handleSubmit.bind(this); 
     this.cancelar = this.cancelar.bind(this);
     this.handleChangeInput = this.handleChangeInput.bind(this);
+    this.handleChangeCaptcha = this.handleChangeCaptcha.bind(this);
+    this.handleChangeCaptchaExpirado = this.handleChangeCaptchaExpirado.bind(this);
   }
 
   handleChangeInput = e => {
@@ -26,6 +33,19 @@ class RecuperarContrasenia extends React.Component {
       [e.target.name] : e.target.value
     });
   }
+
+  handleChangeCaptcha = value => {
+      this.setState({
+        catchaValido: true
+      });
+  };
+
+  handleChangeCaptchaExpirado = value => {
+    this.setState({
+      catchaValido: false
+    });
+}
+
 
   handleSubmit = (event) => {
     // if(this.validarFormulario() == true) {
@@ -46,7 +66,7 @@ class RecuperarContrasenia extends React.Component {
         <span className="input-group-addon"><i className="fa fa-lock"></i></span>
           <input type="text" className="form-control" name="email" 
                               readOnly={this.state.soloLectura}
-                                      defaultValue = {this.state.email} onChange={handleChangeInput}
+                                      defaultValue = {this.state.email} onChange={this.handleChangeInput}
                                       placeholder="Ingrese su email" />	
         </div>
         <span id="emailHelp" className="text-danger error_negrita">
@@ -76,7 +96,69 @@ class RecuperarContrasenia extends React.Component {
     );
   }
 
+  renderContrasenia() {
+    const {handleChangeInput} = this.props;
+    return(
+      <div className="row">
+      <div className="col-lg-6">
+        <div className="form-group">
+          <label htmlFor="password">Contraseña</label>
+          <div className="input-group">
+            <span id="passwordHelp" className="text-danger error_negrita">
+              {this.props.errors["passdistintas"]}
+            </span> 
+          </div>
+          <div className="input-group">
+            <span className="input-group-addon"><i className="fa fa-lock"></i></span>
+              <input type="password" className="form-control" name="password" 
+                                          defaultValue = {this.props.password} onChange={this.handleChangeInput}
+                                          placeholder="Ingrese su contraseña" />	
+            </div>
+            <span id="passwordHelp" className="text-danger error_negrita">
+              {this.props.errors["password"]}
+            </span> 
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <div className="form-group">
+        <label htmlFor="password2">Ingresela de nuevo</label>
+          <div className="input-group">
+            <span className="input-group-addon"><i className="fa fa-lock"></i></span>
+              <input type="password" className="form-control" name="password2" 
+                                          defaultValue = {this.props.password2} onChange={this.handleChangeInput}
+                                          placeholder="Escribala de nuevo" />	
+            </div>
+            <span id="password2Help" className="text-danger error_negrita">
+              {this.props.errors["password2"]}
+            </span> 
+        </div>
+      </div>
+    </div>
+    );
+  }
+  
+  renderCaptcha() {
+    return(
+      <div className="row">
+        <div className="col-lg-6 col-12">
+          <div className="form-group">
+            <ReCAPTCHA
+              sitekey={API_CAPTCHA_PUBLIC}
+              onChange={this.handleChangeCaptcha}
+              onExpired={this.handleChangeCaptchaExpirado}
+            />
+            <span id="passwordHelp" className="text-danger error_negrita">
+              {this.state.errors["captcha"]}
+            </span> 
+          </div>
+        </div> 
+      </div> 
+    );
+  }
+
   render() {
+    if(this.state.isLoading == true)
+      return  <Loading></Loading>
     if(this.state.redirectLogin)
       return <Redirect to = {{ pathname: "/login" }} />;
     return (
@@ -94,6 +176,7 @@ class RecuperarContrasenia extends React.Component {
                     </div>
                     <div className="row justify-content-center">
                       <>{this.renderEmail()}</>
+                      <>{this.renderCaptcha()}</>
                       <>{this.renderBotones()}</>
                     </div>
                 </form>
