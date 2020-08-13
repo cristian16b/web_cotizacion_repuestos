f9d839ff558b771231fc8b954c06d81cbf160cb2
cristian16b <cristian.budzicz@gmail.com>
2020-08-13 17:16:26 -0300

    Se agregan cambios parciales en recuperar contraseña

--- assets/js/Components/recuperar/recuperarContrasenia.js
+++ assets/js/Components/recuperar/recuperarContrasenia.js
@@ -4,11 +4,51 @@ class RecuperarContrasenia extends React.Component {
 
   constructor(props){
     super(props);
+
+    this.state = (
+      {
+        redirectLogin: false,
+      }
+    );
+
+    this.handleSubmit   = this.handleSubmit.bind(this); 
+  }
+
+  handleSubmit = (event) => {
+    // if(this.validarFormulario() == true) {
+    //   this.consumirApiRegister();
+    // } 
+    event.preventDefault();
+  }
+
+  redirectToLogin = () => {
+    this.setState({redirectLogin: true});
   }
 
   render() {
+    if(this.state.redirectLogin)
+      return <Redirect to = {{ pathname: "/login" }} />;
     return (
-      <h1>En construcción!!!</h1>
+    <div className="row justify-content-center">
+      <div className="col-12 col-sm-12 col-md-12 col-lg-8">
+        <div className="card shadow-sm p-3 mb-5 bg-white rounded">
+          <div className="card-body">
+            <h5>Recuperar tu contraseña</h5>
+              <hr></hr>
+                <form  method="post" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                      <span className="text-danger error_negrita">
+                        {this.state.errorApi}
+                      </span>
+                    </div>
+                    <div className="row justify-content-center">
+
+                    </div>
+                </form>
+              </div>
+            </div>
+        </div>
+      </div>
     );
   }
 }
