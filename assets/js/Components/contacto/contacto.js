71eda6c953b6c34eb36d790fa0f38dd577f6deee
cristian16b <cristian.budzicz@gmail.com>
2020-08-14 09:57:34 -0300

    Agregando funcionalidades para hacer el formulario de contacto

--- assets/js/Components/contacto/contacto.js
+++ assets/js/Components/contacto/contacto.js
@@ -1,12 +1,184 @@
 import React , { Component } from 'react';
+import {API_CAMBIAR_CONTRASENIA,API_CAPTCHA_PUBLIC} from '../../Constantes/constantes';
+import axios from 'axios';
+import ReCAPTCHA from "react-google-recaptcha";
+import Loading from '../loading/loading.js';
+import { Redirect } from 'react-router-dom';
 
 class Contacto extends React.Component {
 
   constructor(props){
     super(props);
+
+    this.state = (
+      {
+        redirectLogin: false,
+        // errores locales o retornados por la api
+        errors: {},
+        errorApi: '',
+        email: '',
+        password:'',
+        password2: '',
+        catchaValido: false,
+      }
+    );
+
+    this.handleSubmit   = this.handleSubmit.bind(this); 
+    this.cancelar = this.cancelar.bind(this);
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+    this.handleChangeCaptcha = this.handleChangeCaptcha.bind(this);
+    this.handleChangeCaptchaExpirado = this.handleChangeCaptchaExpirado.bind(this);
+  }
+
+  renderCaptcha() {
+    return(
+      <div className="row">
+        <div className="col-lg-6 col-12">
+          <div className="form-group">
+            <ReCAPTCHA
+              sitekey={API_CAPTCHA_PUBLIC}
+              onChange={this.handleChangeCaptcha}
+              onExpired={this.handleChangeCaptchaExpirado}
+            />
+            <span className="text-danger error_negrita">
+              {this.state.errors["captcha"]}
+            </span> 
+          </div>
+        </div> 
+      </div> 
+    );
+  }
+
+  handleChangeInput = e => {
+    this.setState({
+      [e.target.name] : e.target.value
+    });
+  }
+
+  handleChangeCaptcha = value => {
+      this.setState({
+        catchaValido: true
+      });
+  };
+
+  handleChangeCaptchaExpirado = value => {
+    this.setState({
+      catchaValido: false
+    });
+  }
+
+  validarFormulario = () => {
+    let formularioValido = true;
+    let errors = {};
+
+    if(this.state.email.length == 0) {
+      errors["email"] = "Debe completar su e-mail";
+      formularioValido = false;
+    }
+  
+    if(this.state.password2.length == 0) {
+      errors["password2"] = "Debe ingresar nuevamente la contraseña";
+      formularioValido = false;
+    }
+
+    if(this.state.password.length == 0) {
+      errors["password"] = "La contraseña no puede estar vacia";
+      formularioValido = false;
+    }
+
+    if(this.state.password != this.state.password2) {
+      errors["passdistintas"] = "Las contraseñas ingresadas son diferentes";
+      formularioValido = false;
+    }
+
+    if(this.state.catchaValido == false) {
+      errors["captcha"] = "Debe completar el captcha";
+      formularioValido = false;
+    }
+
+    this.setState({
+      errors: errors
+    });
+  
+    return formularioValido;
+  }
+
+  consumirApiRegister(){
+    this.setState({isLoading:true});
+    this.setState({captcha: false });
+  
+    const payload={
+      "password":this.state.password,
+      "password2":this.state.password2,
+      "email":this.state.email,
+    };
+    // console.log(payload);
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+    };
+    const formData = new FormData();
+    for (let indice in payload) {
+      // console.log(x + '-' + payload[x]);
+      formData.append(indice,payload[indice]);
+    } 
+    // llamamos a la api
+    this.getData(API_CAMBIAR_CONTRASENIA,formData,headers);
+    event.preventDefault();
+  }
+  
+  async getData(url,payload,headers){
+    try 
+    {
+      // Load async data from an inexistent endpoint.
+      const response = await axios.post(url,payload,headers);
+      const { data } = await response;
+      this.setState({ catchaValido: false });
+  
+      let code = response.data.code;
+      if(code == 200){
+        this.setState({ redirectLogin: true }); // after signing up, set the state to true. This will trigger a re-render
+      }
+      else if(code == 400) {
+        this.mostrarErroresApi(response.data.error);
+      }
+      this.setState({isLoading: false});
+    } 
+    catch (e) 
+    {
+      console.log(`😱 Axios request failed: ${e}`);
+      this.setState({isLoading: false});
+      this.setState({ catchaValido: false });
+      alert('Ocurrio un error inesperado, intente nuevamente mas tarde!');
+    }
+  }
+
+  mostrarErroresApi = (response) => {
+    let errors = {};
+    for (const prop in response) {
+      // console.log(`obj.${prop} = ${mensajes[prop]}`);
+      errors[prop] = response[prop];
+    }
+    this.setState({
+      errors: errors
+    });
+  }
+
+  handleSubmit = (event) => {
+    if(this.validarFormulario() == true) {
+      this.consumirApiRegister();
+    } 
+    event.preventDefault();
+  }
+
+  cancelar = () => {
+    this.setState({redirectLogin: true});
   }
 
   render() {
+    if(this.state.isLoading == true)
+      return  <Loading></Loading>
+    if(this.state.redirectLogin)
+      return <Redirect to = {{ pathname: "/contacto" }} />;
     return (        
       <div className="row justify-content-center">
         <div className="col-12 col-sm-12 col-md-12 col-lg-9">
@@ -70,38 +242,11 @@ class Contacto extends React.Component {
                                       <br />
                                       <button type="submit" className="btn btn-primary login-btn btn-block">Enviar</button>
                                     </div>
-
                                 </div>
                             </div>
-
                         </form>
-
-                        
-                        
-                        <div className="status"></div>
                     </div>
-                    {/* <!--Grid column--> */}
-
-                    {/* <!--Grid column--> */}
-                    <div className="col-md-3 text-center">
-                        <ul className="list-unstyled mb-0">
-                            {/* <li><i className="fas fa-map-marker-alt fa-2x"></i>
-                                <p>Santa fe ...</p>
-                            </li>
-
-                            <li><i className="fas fa-phone mt-4 fa-2x"></i>
-                                <p>342 .....</p>
-                            </li> */}
-
-                            <li><i className="fas fa-envelope mt-4 fa-2x"></i>
-                                <p>info@eisenparts.com</p>
-                            </li>
-                        </ul>
-                    </div>
-                    {/* <!--Grid column--> */}
-
-                </div>
-
+                  </div>
                 </section>
               </div>
             </div>
