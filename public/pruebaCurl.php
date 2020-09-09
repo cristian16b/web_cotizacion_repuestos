aaa150d0baf6ff6c0e3c2d0c370f433a548151cc
cristian16b <cristian.budzicz@gmail.com>
2020-09-09 19:35:52 -0300

    Se agrego notas en la documentacion de 'marketplace' y una prueba de usar curl

---
+++ public/pruebaCurl.php
@@ -0,0 +1,23 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.mercadopago.com/oauth/token",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "client_secret=TEST-6864113784926029-082523-64405d2ff4a697e4df1bedc147234d55-167188015&code=TG-5f594ff5192036000650c515-167188015&grant_type=authorization_code&redirect_uri=http%3A//localhost/symfony_mercado_pago/public/index.php/vincular/vendedor",
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/x-www-form-urlencoded"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
