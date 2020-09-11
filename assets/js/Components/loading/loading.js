73049f1f114a7a666c8c905a2cd49d3e9484db37
cristian16b <cristian.budzicz@gmail.com>
2020-09-11 11:41:22 -0300

    modificando los estilos del loading

--- assets/js/Components/loading/loading.js
+++ assets/js/Components/loading/loading.js
@@ -3,7 +3,7 @@ import ReactLoading from 'react-loading';
  
 const divLoading = {
     'height': "100%",
-    'width': "100%",   
+    'width': "100%" 
 }
 
 const divBucle = {
@@ -14,25 +14,43 @@ const divMensaje = {
     'margin': '6% auto'
 }
 
+const centrar = {
+    'display': 'flex',
+    'justifyContent': 'center',
+    'alignItems': 'center',
+}
+
+const centrarH = {
+    'display': 'flex',
+    'alignItems': 'center',
+}
 
 const Loading = () => (
-    <div className="row justify-content-center" style={divLoading}>
-            <div className="row justify-content-center"  align="center" style={divBucle}>
-                <div className="col-8 col-md-8 col-lg-8">
-                    <ReactLoading type={'spin'} color="#153D57" height={'30%'} width={'30%'}/>
+    <div style={divLoading}>
+        <div className="row"  align="center" style={divBucle}>
+            <div className="col-12 col-md-12 col-lg-12">
+                <ReactLoading type={'spin'} color="#153D57" height={'20%'} width={'20%'}/>
+            </div>
+        </div>
+        <br></br>
+        <div style={divMensaje}>
+            <div className="row" style={centrar}>
+                <div className="col-8 col-md-8 col-lg-4">
+                    <h5 style={centrarH}>Cargando</h5>
                 </div>
             </div>
-            <div className="row justify-content-center" align="center" style={divMensaje}>
-                <div className="col-8 col-md-8 col-lg-8">
-                    <h5>Cargando</h5>
-                </div>
-                <div className="col-8 col-md-8 col-lg-8">
+            <div className="row" style={centrar}>
+                <div className="col-8 col-md-8 col-lg-4">
                     <h5>...</h5>
                 </div>
-                <div className="col-8 col-md-8 col-lg-8">
+            </div> 
+            <div className="row" style={centrar}>
+                <div className="col-8 col-md-8 col-lg-4">
                     <h5>Espere un momento</h5>
                 </div>
             </div>
+        </div>
+        <br></br>  
     </div>
 );
  
