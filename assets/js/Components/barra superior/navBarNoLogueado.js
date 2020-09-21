2df125476c703e6690192a6ac5126c86226e9ddb
cristian16b <cristian.budzicz@gmail.com>
2020-09-21 11:33:04 -0300

    Se modifico como se visualiza el navbar no logueado

--- assets/js/Components/barra superior/navBarNoLogueado.js
+++ assets/js/Components/barra superior/navBarNoLogueado.js
@@ -8,13 +8,13 @@ class NavbarNoLogueado extends React.Component {
 
     render() {
         return (
-                <nav className="navbar-custom navbar navbar-expand-md">
+                <nav className="navbar navbar-custom navbar-fixed-top">
                     <Link to="/" className="navbar-brand"><span className="navbar-brand"></span></Link>
                     <button className="navbar-toggler ml-auto custom-toggler" type="button" data-toggle="collapse"  data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav">
+                            <ul className="nav navbar-nav">
                                     {/* 
                                         los siguientes elemetos incrustados en los <li> son para que cuando
                                         se haga click el menu se cierre
@@ -24,16 +24,18 @@ class NavbarNoLogueado extends React.Component {
                                 <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/")}>
                                         <Link to="/" className="navbar-brand"><span className="nav-link">¿Quienes somos?</span></Link>
                                 </li>
-                                <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/login")}>
-                                        <Link to="/login" className="navbar-brand"><span className="nav-link">Ingresar</span></Link>
-                                </li>
                                 <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/registrarme")}>
                                         <Link to="/registrarme" className="navbar-brand"><span className="nav-link">Registrarme</span></Link>
                                 </li>
                                 <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/contacto")}>
                                         <Link to="/contacto" className="navbar-brand"><span className="nav-link">¿Dudas, Consultas?</span></Link>
                                 </li>                
-                    </ul>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li data-toggle="collapse" data-target=".navbar-collapse" className={this.getNavLinkClass("/login")}>
+                                    <Link to="/login" className="navbar-brand"><span className="nav-link">Ingresar</span></Link>
+                                </li>
+                            </ul>
                 </div> 
             </nav>
         )
