35595a3dcec3bcea6727d088d40ccb4066d4e1f3
cristian16b <cristian.budzicz@gmail.com>
2020-09-22 15:59:54 -0300

    Se adapto el home

--- assets/js/Components/home/home.js
+++ assets/js/Components/home/home.js
@@ -11,11 +11,11 @@ class Home extends React.Component {
     return (        
       <div>
         <div className="row justify-content-center">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-9">
-            <h5>Eisen Part</h5>
+          <div className="col-12 col-sm-12 col-md-12 col-lg-11">
             <div className="card shadow-sm p-3 mb-5 bg-white rounded">
             <div className="row justify-content-center">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-9">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-11">
+          <h5>Eisen Part</h5>
               <Carousel>
                 <Carousel.Item>
                   <img
