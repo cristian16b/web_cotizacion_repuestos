dfed6f7b96c97f88b52209f67d37cbd3cdf94411
cristian16b <cristian.budzicz@gmail.com>
2020-09-23 11:35:43 -0300

    Se modifico el home

--- assets/js/Components/home/home.js
+++ assets/js/Components/home/home.js
@@ -7,44 +7,31 @@ class Home extends React.Component {
   constructor(props){
     super(props);
   }
-  // https://i.ibb.co/pzH2yTP/Eisen-Aplicativo03.jpg
+
   render() {
     return (     
       <div>
-        <div className="row justify-content-center altoCarruselImagenes">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-12">
+        <div className="row justify-content-center">
+          <div className="col-8 col-sm-8 col-md-8 col-lg-8">
           <Carousel>
                 <Carousel.Item>
                   <img
                     className="d-block w-100"
                     src="https://www.eisenparts.com/imagenes/Eisen_Aplicativo01.jpg"
-                    alt="First slide"
                   />
-                  {/* <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Carousel.Caption> */}
                 </Carousel.Item>
                 {/* <Carousel.Item>
                   <img
                     className="d-block w-100"
                     src="https://www.eisenparts.com/imagenes/Eisen_Aplicativo02.jpg"
-                    alt="Second slide"
                   />
                 </Carousel.Item> */}
                 <Carousel.Item>
                   <img
                     className="d-block w-100"
                     src="https://www.eisenparts.com/imagenes/Eisen_Aplicativo03.jpg"
-                    alt="Third slide"
                   />
 
-                  {/* <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                  </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
@@ -52,18 +39,11 @@ class Home extends React.Component {
                     src="https://www.eisenparts.com/imagenes/Logotipo_Eisen_Parts.png"
                     alt="Third slide"
                   />
-
-                  {/* <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                  </Carousel.Caption> */}
                 </Carousel.Item>
               </Carousel>
           </div>
         </div>
-        <div className="row justify-content-center">
+        {/* <div className="row justify-content-center">
           <div className="col-12 col-sm-12 col-md-12 col-lg-11">
             <div className="card shadow-sm p-3 mb-5 bg-white rounded">
             <div className="row justify-content-center">
@@ -76,7 +56,7 @@ class Home extends React.Component {
               </div>
             </div>
           </div>
-        </div> 
+        </div>  */}
       </div>
     );
   }
