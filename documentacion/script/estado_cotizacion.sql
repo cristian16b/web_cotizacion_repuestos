3393d73cd913006da904ef626c2a4aadd0381783
cristian16b <cristian.budzicz@gmail.com>
2020-09-28 19:19:32 -0300

    Se anexan los archivos correspondientes para los scripts de importacion de estado cotizacion y estado solicitud

---
+++ documentacion/script/estado_cotizacion.sql
@@ -0,0 +1,10 @@
+
+--
+-- Volcado de datos para la tabla `estado_cotizacion`
+--
+
+INSERT INTO `estado_cotizacion` (`id`, `descripcion`, `fecha_alta`, `fecha_baja`) VALUES
+(1, 'ENVIADA', '2020-08-05', NULL),
+(2, 'ACEPTADA', '0000-00-00', NULL),
+(3, 'RECHAZADA', '0000-00-00', NULL);
+
---
+++ documentacion/script/estado_solicitud.sql
@@ -0,0 +1,10 @@
+
+--
+-- Volcado de datos para la tabla `estado_solicitud`
+--
+
+INSERT INTO `estado_solicitud` (`id`, `descripcion`, `fecha_alta`, `fecha_baja`) VALUES
+(1, 'INICIADA', '2020-08-06', NULL),
+(2, 'CANCELADA', '2020-08-06', NULL),
+(3, 'FINALIZADA', '2020-08-06', NULL);
+
