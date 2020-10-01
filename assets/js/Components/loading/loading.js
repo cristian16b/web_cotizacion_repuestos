81cf2a74082f9bee7e7c59965a61897b8b8d31ed
cristian16b <cristian.budzicz@gmail.com>
2020-10-01 15:31:03 -0300

    Se modifico la visualizacion del loading

--- assets/js/Components/loading/loading.js
+++ assets/js/Components/loading/loading.js
@@ -20,6 +20,14 @@ const centrar = {
     'alignItems': 'center',
 }
 
+const h5 = {
+    'textTransform': 'uppercase',
+    'color': 'rgb(244,179,27)',
+    'backgroundColor': 'rgba(21, 61, 87)',
+    'paddingLeft': '10%',
+    'borderRadius': '25px',
+    'opacity':'0.9',
+}
 
 const Loading = () => (
     <div style={divLoading}>
@@ -32,17 +40,17 @@ const Loading = () => (
         <div style={divMensaje}>
             <div className="row" style={centrar}>
                 <div className="col-8 col-md-8 col-lg-4">
-                    <h5 align="center">Cargando</h5>
+                    <h5 align="center" style={h5}>Cargando</h5>
                 </div>
             </div>
             <div className="row" align="center" style={centrar}>
                 <div className="col-8 col-md-8 col-lg-4">
-                    <h5>...</h5>
+                    <h5 style={h5}>...</h5>
                 </div>
             </div> 
             <div className="row" align="center" style={centrar}>
                 <div className="col-8 col-md-8 col-lg-4">
-                    <h5>Espere un momento</h5>
+                    <h5 style={h5}>Espere un momento</h5>
                 </div>
             </div>
         </div>
