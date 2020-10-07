89a5c87ef7b544f0fb8121f0ed9bc2eec6519d3c
cristian16b <cristian.budzicz@gmail.com>
2020-10-07 12:08:00 -0300

    Se esta refactorizando el codigo para incluir un nuevo endpoint

--- src/Controller/ListarCotizacionesController.php
+++ src/Controller/ListarCotizacionesController.php
@@ -5,15 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/api/v1/listar/cotizaciones")
+*/
 class ListarCotizacionesController extends AbstractController
 {
-    /**
-     * @Route("/listar/cotizaciones", name="listar_cotizaciones")
-     */
-    public function index()
-    {
-        return $this->render('listar_cotizaciones/index.html.twig', [
-            'controller_name' => 'ListarCotizacionesController',
-        ]);
-    }
+    // /**
+    //  * @Route("/listar/cotizaciones", name="listar_cotizaciones")
+    //  */
+    // public function index()
+    // {
+    //     return $this->render('listar_cotizaciones/index.html.twig', [
+    //         'controller_name' => 'ListarCotizacionesController',
+    //     ]);
+    // }
 }
